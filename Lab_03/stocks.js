20e20fb9abe6434f6379fcd923a6661e3dfa0519
PMARINA <bluerat33@gmail.com>
2021-09-25 17:20:20 -0400

    Lab3: Complete findShareholder method
    - In case I need to go quickly
    - Might rebase at some point into the future

--- Lab_03/stocks.js
+++ Lab_03/stocks.js
@@ -31,12 +31,44 @@ async function listShareholders() {
   }
   return retStockData;
 }
+
+/**
+ * Return who the top shareholder of the stock is
+ * @param {string} stockName The name of the stock
+ * @return {string} A top shareholder of the stock
+ */
+async function topShareholder(stockName) {
+  if (typeof(stockName)!== 'string') throw Error('bad input');
+  if ( stockName.trim().length === 0) throw Error('empty input');
+  const shareholders = await listShareholders();
+  for (const stock of shareholders) {
+    const iteratingStockName = stock.stock_name;
+    if (iteratingStockName === stockName) {
+      const holders = stock.shareholders;
+      if (holders.length === 0) return `${iteratingStockName} currently has no shareholders.`;
+      let numShares = 0;
+      let topHolder = null;
+      for (const holder of holders) {
+        if (holder.number_of_shares > numShares) {
+          numShares = holder.number_of_shares;
+          topHolder = `${holder.first_name} ${holder.last_name}`;
+        }
+      }
+      return `With ${numShares} shares in ${iteratingStockName}, ${topHolder} is the top shareholder.`;
+    }
+  }
+  throw Error('No stock with that name');
+}
+
+
 // module.exports = {listShareholders, topShareholders, listStocks, getStockById};
 /**
  * Do something
  */
 async function main() {
-  const something = await listShareholders().catch((e)=>console.log(e));
+  const something = await topShareholder('Aeglea BioTherapeutics, Inc.').catch((e)=>console.log(e));
+  console.log(something);
+  return;
   // <stackOverflowSnippet src="https://stackoverflow.com/a/10729284">
   const util = require('util');
   console.log(util.inspect(something, {showHidden: false, depth: null, colors: true}));
